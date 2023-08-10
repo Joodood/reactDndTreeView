a94c2272802ad4f74863a6947141341d1c1d35dc
Jude Hathway <jude.hathway@gmail.com>
2023-08-10 15:11:25 -0400

    Configure prototype react DndTreeView

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { theme } from "./theme";
+import { DndProvider } from "react-dnd";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { MultiBackend, getBackendOptions } from "@minoru/react-dnd-treeview";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <DndProvider backend={MultiBackend} options={getBackendOptions()}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+        </DndProvider>
+    </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
